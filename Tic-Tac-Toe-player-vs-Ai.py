7c93965297c12e81b423928c908b131221fc1c37
iragut <74363823+iragut@users.noreply.github.com>
2022-09-21 01:16:33 +0300

    Add medium Ai

--- Tic-Tac-Toe-player-vs-Ai.py
+++ Tic-Tac-Toe-player-vs-Ai.py
@@ -9,84 +9,157 @@ def output_print(list_output):
 ---------''')
 
 
-game = 0
-game_output = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
-output_print(game_output)
-occupied_cell = []
-status = "player"
+def winning(data_list):
+    if (data_list[0] + data_list[1] + data_list[2]) == "XXX" or (
+            data_list[3] + data_list[4] + data_list[5]) == "XXX" or (
+            data_list[6] + data_list[7] + data_list[8]) == "XXX":
+        print("X wins")
+        return True
+    elif (data_list[0] + data_list[3] + data_list[6]) == "XXX" or (
+            data_list[1] + data_list[4] + data_list[7]) == "XXX" or (
+            data_list[2] + data_list[5] + data_list[8]) == "XXX":
+        print("X wins")
+        return True
+    elif (data_list[0] + data_list[4] + data_list[8]) == "XXX" or (
+            data_list[2] + data_list[4] + data_list[6]) == "XXX":
+        print("X wins")
+        return True
+    elif (data_list[0] + data_list[3] + data_list[6]) == "OOO" or (
+            data_list[1] + data_list[4] + data_list[7]) == "OOO" or (
+            data_list[2] + data_list[5] + data_list[8]) == "OOO":
+        print("O wins")
+        return True
+    elif (data_list[0] + data_list[1] + data_list[2]) == "OOO" or (
+            data_list[3] + data_list[4] + data_list[5]) == "OOO" or (
+            data_list[6] + data_list[7] + data_list[8]) == "OOO":
+        print("O wins")
+        return True
+    elif (data_list[0] + data_list[4] + data_list[8]) == "OOO" or (
+            data_list[2] + data_list[4] + data_list[6]) == "OOO":
+        print("O wins")
+        return True
 
-while game < 9:
-    index_map = 0
-    if status == "player":
-        while True:
-            player_input = input("Enter the coordinates:").split()
-            if len(player_input) != 2:
-                print("You should enter numbers!")
-            else:
-                rows = player_input[0]
-                column = player_input[1]
-                break
-        counter = False
-        
-        while True:
-            if not rows.isdigit() and not column.isdigit():
-                print("You should enter numbers!")
-                break
-            elif int(rows) > 3 or int(column) > 3:
-                print("Coordinates should be from 1 to 3!")
-                break
-            else:
-                index_map = ((int(rows) - 1) * 3 + (int(column) + 2)) - 3
 
-                for a in occupied_cell:
-                    if a == index_map:
-                        print("This cell is occupied! Choose another one!")
-                        counter = True
+class PlayerVsAi:
+    def __init__(self, difficulty):
+        game = 0
+        game_output = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
+        output_print(game_output)
+        occupied_cell = []
+        status = "player"
+
+        while game < 9:
+            if status == "player":
+                while True:
+                    player_input = input("Enter the coordinates:").split()
+                    if len(player_input) != 2:
+                        print("You should enter numbers!")
+                    else:
+                        rows = player_input[0]
+                        column = player_input[1]
+                        counter = False
                         break
 
-                if counter:
+                while True:
+                    if not rows.isdigit() and not column.isdigit():
+                        print("You should enter numbers!")
+                        break
+                    elif int(rows) > 3 or int(column) > 3:
+                        print("Coordinates should be from 1 to 3!")
+                        break
+                    else:
+                        index_map = ((int(rows) - 1) * 3 + (int(column) + 2)) - 3
+
+                        for a in occupied_cell:
+                            if a == index_map:
+                                print("This cell is occupied! Choose another one!")
+                                counter = True
+                                break
+
+                        if counter:
+                            break
+                        for i in range(0, len(game_output)):
+                            if i == index_map:
+                                game_output.pop(i)
+                                game_output.insert(i, "X")
+                                output_print(game_output)
+                                occupied_cell.append(i)
+                                break
+                        game += 1
+                        status = "computer"
+                        break
+            else:
+                if difficulty == "easy":
+                    while True:
+                        computer_choice = random.randint(0, 8)
+                        if computer_choice in occupied_cell:
+                            pass
+                        else:
+                            game_output.pop(computer_choice)
+                            game_output.insert(computer_choice, "O")
+                            print("Making move level \"easy\"")
+                            output_print(game_output)
+                            occupied_cell.append(computer_choice)
+                            game += 1
+                            status = "player"
+                            break
+
+                elif difficulty == "medium":
+                    computer_choice = (2 if (game_output[0] + game_output[1] + game_output[2]) == "XX_"
+                                       else 1 if game_output[0] + game_output[1] + game_output[2] == "X_X"
+                                       else 0 if game_output[0] + game_output[1] + game_output[2] == "_XX"
+                                       else 5 if game_output[3] + game_output[4] + game_output[5] == "XX_"
+                                       else 4 if game_output[3] + game_output[4] + game_output[5] == "X_X"
+                                       else 3 if game_output[3] + game_output[4] + game_output[5] == "_XX"
+                                       else 8 if game_output[6] + game_output[7] + game_output[8] == "XX_"
+                                       else 7 if game_output[6] + game_output[7] + game_output[8] == "X_X"
+                                       else 6 if game_output[6] + game_output[7] + game_output[8] == "_XX"
+
+                                       else 6 if game_output[0] + game_output[3] + game_output[6] == "XX_"
+                                       else 3 if game_output[0] + game_output[3] + game_output[6] == "X_X"
+                                       else 0 if game_output[0] + game_output[3] + game_output[6] == "_XX"
+                                       else 7 if game_output[1] + game_output[4] + game_output[7] == "XX_"
+                                       else 4 if game_output[1] + game_output[4] + game_output[7] == "X_X"
+                                       else 1 if game_output[1] + game_output[4] + game_output[7] == "_XX"
+                                       else 8 if game_output[2] + game_output[5] + game_output[8] == "XX_"
+                                       else 5 if game_output[2] + game_output[5] + game_output[8] == "X_X"
+                                       else 2 if game_output[2] + game_output[5] + game_output[8] == "_XX"
+
+                                       else 8 if game_output[0] + game_output[4] + game_output[8] == "XX_"
+                                       else 4 if game_output[0] + game_output[4] + game_output[8] == "X_X"
+                                       else 0 if game_output[0] + game_output[4] + game_output[8] == "_XX"
+                                       else 6 if (game_output[2] + game_output[4] + game_output[6]) == "XX_"
+                                       else 4 if (game_output[2] + game_output[4] + game_output[6]) == "X_X"
+                                       else 2 if (game_output[2] + game_output[4] + game_output[6]) == "_XX" else random.randint(0, 8))
+
+                    while True:
+                        if computer_choice in occupied_cell:
+                            computer_choice = random.randint(0, 8)
+                        else:
+                            game_output.pop(computer_choice)
+                            game_output.insert(computer_choice, "O")
+                            print("Making move level \"medium\"")
+                            output_print(game_output)
+                            occupied_cell.append(computer_choice)
+                            game += 1
+                            status = "player"
+                            break
+
+                if winning(game_output):
                     break
-                for i in range(0, len(game_output)):
-                    if i == index_map:
-                        game_output.pop(i)
-                        game_output.insert(i, "X")
-                        output_print(game_output)
-                        occupied_cell.append(i)
-                        break
-                game += 1
-                status = "computer"
-                break
-    else:
-        computer_choice = random.randint(0, 8)
-        if computer_choice in occupied_cell:
-            computer_choice = random.randint(0, 8)
-        else:
-            game_output.pop(computer_choice)
-            game_output.insert(computer_choice, "O")
-            print("Making move level \"easy\"")
-            output_print(game_output)
-            occupied_cell.append(computer_choice)
-            game += 1
-            status = "player"
 
-        if (game_output[0] + game_output[1] + game_output[2]) == "XXX" or (game_output[3] + game_output[4] + game_output[5]) == "XXX" or (game_output[6] + game_output[7] + game_output[8]) == "XXX":
-            print("X wins")
-            break
-        elif (game_output[0] + game_output[3] + game_output[6]) == "XXX" or (game_output[1] + game_output[4] + game_output[7]) == "XXX" or (game_output[2] + game_output[5] + game_output[8]) == "XXX":
-            print("X wins")
-            break
-        elif (game_output[0] + game_output[4] + game_output[8]) == "XXX" or (game_output[2] + game_output[4] + game_output[6]) == "XXX":
-            print("X wins")
-            break
-        elif (game_output[0] + game_output[3] + game_output[6]) == "OOO" or (game_output[1] + game_output[4] + game_output[7]) == "OOO" or (game_output[2] + game_output[5] + game_output[8]) == "OOO":
-            print("O wins")
-            break
-        elif (game_output[0] + game_output[1] + game_output[2]) == "OOO" or (game_output[3] + game_output[4] + game_output[5]) == "OOO" or (game_output[6] + game_output[7] + game_output[8]) == "OOO":
-            print("O wins")
-            break
-        elif (game_output[0] + game_output[4] + game_output[8]) == "OOO" or (game_output[2] + game_output[4] + game_output[6]) == "OOO":
-            print("O wins")
-            break
+        if game == 9:
+            print("Draw")
 
-if game == 9:
-    print("Draw")
+
+while True:
+    input_command = input("Input command:").split()
+    if input_command[0] == "exit":
+        break
+    elif len(input_command) < 3:
+        print("Bad parameters!")
+    elif "easy" in input_command:
+        PlayerVsAi("easy")
+    elif "medium" in input_command:
+        PlayerVsAi("medium")
+
